52b9d455fc844791964f60c798932bc9aeac7c37
alifianadexe <alifianadexe@gmail.com>
2022-12-17 22:18:47 +0700

    build off eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
-}
+};
