7f661370b772dcbb308e203da0617bce745cd301
alifianadexe <alifianadexe@gmail.com>
2022-12-28 20:12:15 +0700

    fix bug'

--- components/organisme/Reached/index.tsx
+++ components/organisme/Reached/index.tsx
@@ -1,33 +1,49 @@
-
 export default function Reached() {
-    return (
-        <section className="reached pt-50 pb-50">
-            <div className="container-fluid">
-                <div className="d-flex flex-lg-row flex-column align-items-center justify-content-center gap-lg-0 gap-4">
-                    <div className="me-lg-35">
-                        <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">10.000+</p>
-                        <p className="text-lg text-lg-start text-center color-palette-6 m-0">All Time Transaction</p>
-                    </div>
-                    <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-                    <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-                    <div className="me-lg-35 ms-lg-35">
-                        <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0"> >500</p>
-                        <p className="text-lg text-lg-start text-center color-palette-6 m-0">Customer</p>
-                    </div>
-                    <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-                    <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-                    <div className="me-lg-35 ms-lg-35">
-                        <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">99,9%</p>
-                        <p className="text-lg text-lg-start text-center color-palette-6 m-0">Happy Players</p>
-                    </div>
-                    <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-                    <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-                    <div className="me-lg-35 ms-lg-35">
-                        <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0"> Since 2019</p>
-                        <p className="text-lg text-lg-start text-center color-palette-6 m-0">We Serve</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="reached pt-50 pb-50">
+      <div className="container-fluid">
+        <div className="d-flex flex-lg-row flex-column align-items-center justify-content-center gap-lg-0 gap-4">
+          <div className="me-lg-35">
+            <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
+              10.000+
+            </p>
+            <p className="text-lg text-lg-start text-center color-palette-6 m-0">
+              All Time Transaction
+            </p>
+          </div>
+          <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
+          <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
+          <div className="me-lg-35 ms-lg-35">
+            <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
+              500
+            </p>
+            <p className="text-lg text-lg-start text-center color-palette-6 m-0">
+              Customer
+            </p>
+          </div>
+          <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
+          <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
+          <div className="me-lg-35 ms-lg-35">
+            <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
+              99,9%
+            </p>
+            <p className="text-lg text-lg-start text-center color-palette-6 m-0">
+              Happy Players
+            </p>
+          </div>
+          <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
+          <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
+          <div className="me-lg-35 ms-lg-35">
+            <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
+              {" "}
+              Since 2019
+            </p>
+            <p className="text-lg text-lg-start text-center color-palette-6 m-0">
+              We Serve
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/organisme/Story/index.tsx
+++ components/organisme/Story/index.tsx
@@ -1,26 +1,54 @@
+import { MDBIcon } from "mdb-react-ui-kit";
 
 export default function Story() {
-    return (
-        <section className="story pt-50 pb-50">
-            <div className="container-xxl container-fluid">
-                <div className="row align-items-center px-lg-5 mx-auto gap-lg-0 gap-4">
-                    <div className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60" data-aos="zoom-in">
-                        <img src="/img/Header-9.png" width="612" height="452" className="img-fluid" alt=""/>
-                    </div>
-                    <div className="col-lg-5 col-12 ps-lg-60">
-                        <div className="">
-                            <h2 className="text-4xl fw-bold color-palette-1 mb-30">Win the battle.<br/> Be the Champion.
-                            </h2>
-                            <p className="text-lg color-palette-1 mb-30">Kami menyediakan jutaan cara untuk<br
-                                    className="d-sm-block d-none"/>
-                                membantu players menjadi<br className="d-sm-block d-none"/> pemenang sejati</p>
-                            <div className="d-md-block d-flex flex-column w-100">
-                                <a className="btn btn-read text-lg rounded-pill" href="#" role="button">Read Story</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="story pt-50 pb-50">
+      <div className="container-xxl container-fluid">
+        <div className="row align-items-center px-lg-5 mx-auto gap-lg-0 gap-4">
+          <div
+            className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60"
+            data-aos="zoom-in"
+          >
+            <img
+              src="/img/Header-9.png"
+              width="612"
+              height="452"
+              className="img-fluid"
+              alt=""
+            />
+          </div>
+          <div className="col-lg-5 col-12 ps-lg-60">
+            <div className="">
+              <h2 className="text-4xl fw-bold color-palette-1 mb-30">
+                Win the battle.
+                <br /> Be the Champion.
+              </h2>
+              <p className="text-lg color-palette-1 mb-30">
+                Kami menyediakan jutaan cara untuk
+                <br className="d-sm-block d-none" />
+                membantu players menjadi
+                <br className="d-sm-block d-none" /> pemenang sejati
+              </p>
+              <div className="d-md-block d-flex flex-column w-100">
+                <a
+                  className="btn btn-read text-lg rounded-pill"
+                  target="_blank"
+                  href="https://api.whatsapp.com/send?phone=628981234567&text=Halo%20mau%20order%20gan"
+                  role="button"
+                >
+                  <MDBIcon
+                    className="ms-1 "
+                    icon="camera-retro"
+                    fixed
+                    iconType="solid"
+                  />
+                  Contact
+                </a>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
