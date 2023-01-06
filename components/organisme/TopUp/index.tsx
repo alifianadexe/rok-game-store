7585c5f3ed77c73ffc0ce7dbce15c5e9b7914d58
alifianadexe <alifianadexe@gmail.com>
2023-01-06 13:27:43 +0700

    fixing

--- components/organisme/TopUp/index.tsx
+++ components/organisme/TopUp/index.tsx
@@ -1,6 +1,58 @@
 import Illustration from "./Illustration";
+import React from "react";
+
+const dataRow = [
+  [
+    {
+      title: "Nama Bundle",
+      image: "/img/rok1.1.png",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+    {
+      title: "Nama Bundle",
+      image: "/gamber.jpg",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+    {
+      title: "Nama Bundle",
+      image: "/gamber.jpg",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+  ],
+  [
+    {
+      title: "Nama Bundle",
+      image: "/gamber.jpg",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+    {
+      title: "Nama Bundle",
+      image: "/gamber.jpg",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+    {
+      title: "Nama Bundle",
+      image: "/gamber.jpg",
+      price: "$90",
+      price2: "$91",
+      status: "available",
+    },
+  ],
+];
 
 export default function TopUp() {
+  const [index, setIndex] = React.useState(0);
+
   return (
     <section id="topup" className="header pt-50 pb-50">
       <div className="container-fluid">
@@ -10,222 +62,77 @@ export default function TopUp() {
           <div className="col-lg-6 col-sm-12 d-lg-block">
             <div className="latest-transaction">
               <div className="main-content main-content-table overflow-auto">
-                <table className="table table-borderless">
-                  <thead>
-                    <tr className="color-palette-1">
-                      <th className="text-start" scope="col">
-                        Category
-                      </th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="align-middle">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok1.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            New World Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok2.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            Call of The Ancients Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok3.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            City of Hope Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok4.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            Fountain Wisdom Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok6.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            Living Legend Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok7.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            War Machine Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="align-middle text-center">
-                      <th scope="row">
-                        <img
-                          className="float-start me-3 mb-lg-0 mb-3"
-                          src="/img/rok8.1.png"
-                          width="80"
-                          height="60"
-                          alt=""
-                        />
-                        <div className="game-title-header">
-                          <p className="game-title fw-medium text-start color-palette-9 m-0">
-                            Kings Coronation Bundle
-                          </p>
-                        </div>
-                      </th>
-                      <td>
-                        <p className="fw-medium text-start color-palette-7 m-0">
-                          <sup>$</sup>3.85 - <sup>$</sup>77 USD
-                        </p>
-                      </td>
-                      <td>
-                        <div>
-                          <span className="float-start icon-status success"></span>
-                          <p className="fw-medium text-start color-palette-8 m-0 position-relative">
-                            Available
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+                <div className="slideshow">
+                  <div
+                    className="slideshowSlider"
+                    style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+                  >
+                    {dataRow.map((data, index) => (
+                      <div className="slide" key={index}>
+                        <table className="table table-borderless">
+                          <thead>
+                            <tr className="color-palette-1">
+                              <th className="text-start" scope="col">
+                                Category
+                              </th>
+                              <th scope="col">Price</th>
+                              <th scope="col">Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {data.map((data, index) => (
+                              <tr className="align-middle tex-center">
+                                <th scope="row">
+                                  <img
+                                    className="float-start me-3 mb-lg-0 mb-3"
+                                    src={data.image}
+                                    width="80"
+                                    height="60"
+                                    alt=""
+                                  />
+                                  <div className="game-title-header">
+                                    <p className="game-title fw-medium text-start color-palette-9 m-0">
+                                      {data.title}
+                                    </p>
+                                  </div>
+                                </th>
+                                <td>
+                                  <p className="fw-medium text-start color-palette-7 m-0">
+                                    <sup>$</sup>
+                                    {data.price} - <sup>$</sup>
+                                    {data.price} USD
+                                  </p>
+                                </td>
+                                <td>
+                                  <div>
+                                    <span className="float-start icon-status success"></span>
+                                    <p className="fw-medium text-start color-palette-8 m-0 position-relative">
+                                      {data.status}
+                                    </p>
+                                  </div>
+                                </td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="slideshowDots">
+                    {dataRow.map((_, idx) => (
+                      <div
+                        key={idx}
+                        className={`slideshowDot${
+                          index === idx ? " active" : ""
+                        }`}
+                        onClick={() => {
+                          setIndex(idx);
+                        }}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
